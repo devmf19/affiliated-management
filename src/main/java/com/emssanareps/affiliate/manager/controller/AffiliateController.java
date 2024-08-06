2e94b39c6e0afe9c2b68fdded34348285ce07ac4
Francisco Martinez <franma.martinez19@gmail.com>
2024-08-06 15:58:17 -0500

    third commit

--- src/main/java/com/emssanareps/affiliate/manager/controller/AffiliateController.java
+++ src/main/java/com/emssanareps/affiliate/manager/controller/AffiliateController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/author")
+@RequestMapping("/affiliate")
 public class AffiliateController {
 
     private final AffiliateService affiliateService;
